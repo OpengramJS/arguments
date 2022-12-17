8c59bfc9df98ba3718c136cd688b20c62dfa31e2
viiprogrammer <m_telega@mail.ru>
2022-12-17 17:47:27 +0300

    fix: fix crash without entities, undefined args object

--- src/index.js
+++ src/index.js
@@ -27,7 +27,13 @@ const joiConfig = {
  * @return {function}
  */
 function argumentsParserFactory (parameters = {}) {
-  const { mapping = [], schema, config, errorHandler, allowedUpdates = ['message', 'channel_post', 'inline_query'] } = parameters
+  const {
+    mapping = [],
+    schema,
+    config,
+    errorHandler,
+    allowedUpdates = ['message', 'channel_post', 'inline_query']
+  } = parameters
 
   /**
    * Middleware for parsing & validating command arguments
@@ -37,47 +43,46 @@ function argumentsParserFactory (parameters = {}) {
   const middeware = (ctx, next) => {
     const { updateType, update } = ctx
 
-    if ((!ctx.anyText || ctx.anyEntities?.[0].type !== 'bot_command') && update[updateType].query === undefined) {
-      return next()
-    }
-
     let remapped = {}
     let args = []
-    const text = update[updateType].text ?? update[updateType].query
 
-    if (updateType === 'inline_query' && text) {
-      args = text.split(' ')
-    } else {
-      const match = text.match(/^\/\S+\s?(.+)?/) ?? update[updateType].query
+    if ((ctx.anyText && ctx.anyEntities?.[0]?.type === 'bot_command') || update[updateType].query !== undefined) {
+      const text = update[updateType].text ?? update[updateType].query
 
-      if (match !== null && match[1] !== undefined) {
-        args = match[1].split(' ')
+      if (updateType === 'inline_query' && text) {
+        args = text.split(' ')
+      } else {
+        const match = text.match(/^\/\S+\s?(.+)?/) ?? update[updateType].query
+
+        if (match?.[1] !== undefined) {
+          args = match[1].split(' ')
+        }
       }
-    }
 
-    if (mapping.length) {
-      // Remap by mapping names
-      for (let i = 0; i < mapping.length; i++) {
-        remapped[
-          mapping[i]
-        ] = args[i]
+      if (mapping.length) {
+        // Remap by mapping names
+        for (let i = 0; i < mapping.length; i++) {
+          remapped[
+            mapping[i]
+          ] = args[i]
+        }
       }
-    }
 
-    if (schema && mapping) {
-      const { value, error } = schema.validate(remapped, { ...joiConfig, ...config })
+      if (schema && mapping) {
+        const { value, error } = schema.validate(remapped, { ...joiConfig, ...config })
 
-      if (error) {
-        if (errorHandler) {
-          return Promise.resolve(
-            errorHandler(error, ctx)
-          )
+        if (error) {
+          if (errorHandler) {
+            return Promise.resolve(
+              errorHandler(error, ctx)
+            )
+          }
+
+          throw error
         }
 
-        throw error
+        remapped = value
       }
-
-      remapped = value
     }
 
     ctx.state.args = {
